9e73fc414d0357186b4e564c1626162454fc5f18
padamv <padamv91@gmail.com>
2021-02-05 17:50:21 +0100

    Inject mock with annotation

--- unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImplMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImplMockTest.java
@@ -3,21 +3,24 @@ package com.in28minutes.unittesting.unittesting.business;
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
 
-    SomeBusinessImpl business = new SomeBusinessImpl();
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
+    @InjectMocks
+    SomeBusinessImpl business;
 
-    @BeforeEach
-    public void before(){
-        business.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    SomeDataService dataServiceMock;
 
     @Test
     void calculateSumUsingDataService_basic() {

1f4220771c6a4fe0ca47f61f6f78d593cf5cce2a
padamv <padamv91@gmail.com>
2021-02-06 14:08:03 +0100

    Mocking the HelloWorldController

---
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldControllerTest.java
@@ -0,0 +1,33 @@
+package com.in28minutes.unittesting.unittesting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorldBasic() throws Exception {
+        // call "/hello-world" GET application/json
+        RequestBuilder request = MockMvcRequestBuilders
+                                    .get("/hello-world")
+                                    .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request).andReturn();
+
+        // verify "Hello World"
+        assertEquals("Hello World", result.getResponse().getContentAsString());
+
+    }
+}

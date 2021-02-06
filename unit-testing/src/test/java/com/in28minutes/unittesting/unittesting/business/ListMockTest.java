6e032b0a40a854e7736bc4c8b6151beebee70303
padamv <padamv91@gmail.com>
2021-02-06 11:51:54 +0100

    Capture multiple passed variables

--- unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -66,4 +66,19 @@ public class ListMockTest {
 
         assertEquals("SomeString", captor.getValue());
     }
+
+    @Test
+    public void multipleArgumentCapturing() {
+        //SUT
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+
+        //Verification
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+        assertEquals("SomeString1", allValues.get(0));
+        assertEquals("SomeString2", allValues.get(1));
+    }
 }

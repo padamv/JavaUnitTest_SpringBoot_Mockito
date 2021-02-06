c0f3498b3a1a4054523c07c3759d377b7d772e7e
padamv <padamv91@gmail.com>
2021-02-06 11:44:22 +0100

    Verification with times, atLeast atMost and never

--- unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -6,11 +6,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
-    List mock = mock(List.class);
+    List<String> mock = mock(List.class);
 
 
     @Test
@@ -40,4 +39,20 @@ public class ListMockTest {
         assertEquals("in28Minutes", mock.get(1));
     }
 
+    @Test
+    public void verificationBasics(){
+        //SUT
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        //Verify
+        verify(mock).get(0);
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atLeastOnce()).get(anyInt());
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
+
+
+    }
 }

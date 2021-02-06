55c214fd7cffd3038843e989644e697811a21762
padamv <padamv91@gmail.com>
2021-02-06 11:31:58 +0100

    Mocking with anzInt parameter

--- unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,4 +32,12 @@ public class ListMockTest {
         assertEquals("in28Minutes", mock.get(0));
         assertEquals(null, mock.get(1));
     }
+
+    @Test
+    public void returnWithGenericParameters(){
+        when(mock.get(anyInt())).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+        assertEquals("in28Minutes", mock.get(1));
+    }
+
 }

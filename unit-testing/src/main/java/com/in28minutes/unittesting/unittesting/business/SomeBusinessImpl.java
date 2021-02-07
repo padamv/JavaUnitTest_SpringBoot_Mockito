e8271cf678157930b29108c4a6ccadce75a8a9b5
padamv <padamv91@gmail.com>
2021-02-07 14:26:58 +0100

    Refactor to functional java

--- unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
+++ unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -2,6 +2,9 @@ package com.in28minutes.unittesting.unittesting.business;
 
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class SomeBusinessImpl {
 
    private SomeDataService someDataService;
@@ -11,19 +14,11 @@ public class SomeBusinessImpl {
     }
 
     public  int calculateSum(int[] data){
-        int sum = 0;
-        for(int value:data){
-            sum += value;
-        }
-        return sum;
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
     }
 
     public int calculateSumUsingDataService(){
-        int sum = 0;
         int[] data = someDataService.retrieveAllData();
-        for(int value:data){
-            sum += value;
-        }
-        return sum;
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
     }
 }

9ec2e450c5914558eff3edbeefcac07954ea3fc2
padamv <padamv91@gmail.com>
2021-02-07 13:05:54 +0100

    Add entity annotation to item and create record in db

--- unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
+++ unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
@@ -1,12 +1,23 @@
 package com.in28minutes.unittesting.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
+
+    @Id
     private int id;
     private String name;
     private int price;
     private int quantity;
 
+    @Transient
+    private int value;
+
     public Item(int id, String name, int price, int quantity) {
+
         this.id = id;
         this.name = name;
         this.price = price;
@@ -43,6 +54,14 @@ public class Item {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     @Override
